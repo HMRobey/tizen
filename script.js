a0718bef3b205128f3ada7e001d3e7b86a57a76f
HMRobey <harryrobey@gmail.com>
2026-02-16 15:46:08 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("cast").onclick = function() {
+    window.location.href = "knox-casting://";
+};
+
+document.getElementById("hdmi1").onclick = function() {
+    window.location.href = "samsungtv://source/hdmi1";
+};
+
+document.getElementById("hdmi2").onclick = function() {
+    window.location.href = "samsungtv://source/hdmi2";
+};
